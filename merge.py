23f958a6aeec6e2fff47989cfca4af1d92cc3444
Evan Huang <evanhuang117@gmail.com>
2021-02-24 15:28:09 -0500

    implement basic merging of multiple PDFs in a given directory, sorting with natural sort

---
+++ merge.py
@@ -0,0 +1,42 @@
+import os
+import natsort
+from PyPDF2 import PdfFileMerger
+
+#TODO: add option to specify order of merging by outputting list of names
+# and asking user to enter csv list of array indices
+
+# USAGE
+# 1. activate python venv
+# 2. python3 merge.py
+
+def get_dir():
+    dir = input("Enter the full directory path of pdfs you want to merge: ")
+    # check for proper formatting of dir
+    if dir[-1] != '/':
+        dir += '/'
+    # expand ~ for user home directory, needed because python is processing the dir string
+    return os.path.expanduser(dir)
+
+def get_files(dir):
+    # python sorted() doesnt do alphanumeric (natural) sort
+    alphanum_sorted = natsort.natsorted([name for name in os.listdir(dir) if name.endswith(".pdf")])
+    return [dir + name for name in alphanum_sorted if name.endswith(".pdf")]
+
+def merge_files(paths):
+    merger = PdfFileMerger()
+    for pdf in paths:
+        print("Merging: " + pdf)
+        merger.append(pdf);
+    return merger
+
+if __name__ == '__main__':
+    dir = get_dir()
+    pdf_files = get_files(dir)
+    merged = merge_files(pdf_files)
+    # write the entire merged pdf then close the merger
+    # last / in dir name causes an empty string at end of list so filter it out
+    parent_dir = list(filter(None, dir.split('/')))[-1]
+    output_name = dir + parent_dir + "-merged" + ".pdf"
+    print("Writing merged PDF to: " + output_name)
+    merged.write(output_name)
+    merged.close()
